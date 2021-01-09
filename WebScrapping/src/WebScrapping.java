0da1024d7c1c4886bfd98eec39aeab2d370543a3
Janos Bakos <bakos.soda@gmail.com>
2021-01-09 13:38:15 +0100

    removed

--- WebScrapping/src/WebScrapping.java
+++ WebScrapping/src/WebScrapping.java
@@ -2,30 +2,42 @@ import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class WebScrapping {
 
-    public static void main(String [] args){
+    public static void main(String [] args) throws IOException {
 
-        final String url = "http://books.toscrape.com/";
+        final String url = "https://scrapethissite.com/pages/advanced/?gotcha=login";
+        // FileWriter out = new FileWriter(new File("orszagok.txt"));
+
+
 
         try{
             final Document document = Jsoup.connect(url).get();
 
-            for(Element row : document.select("ol.row li")){
-                if(row.select("h3").text().equals(""))
-                {
-                    continue;
-                } else {
-                    final String ticker = row.select("h3").text();
+            for(Element row : document.select("div.col-md-4.col-md-offset-4 form")){
 
+                if(row.select("input.form-control:nth-of-type(1)").text().equals(""))
+                {
+
+                    continue;
+
+                } else {
+
+                    final String ticker = row.select("input.form-control:nth-of-type(1)").text();
+                    // out.write(ticker);
+                    // out.write("\n");
                     System.out.println(ticker);
                 }
             }
-
         }
         catch(Exception e){
             e.printStackTrace();
         }
-
+           // out.close();
     }
 }
--- webscrapping_psStore/src/ws.java
+++ webscrapping_psStore/src/ws.java
@@ -1,38 +1,29 @@
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
-import java.io.EOFException;
 
 public class ws {
 
-    public static void main(String [] args) throws EOFException{
+    public static void main(String [] args){
 
-        final String url = "https://store.playstation.com/en-hu/category/44d8bb20-653e-431e-8ad0-c0a365f68d2f/1";
+        final String url = "https://www.youtube.com/";
 
         try{
-
             final Document document = Jsoup.connect(url).get();
 
-            for(Element row : document.select("ul.ems-sdk-product-tile-list.psw-grid-x.psw-grid-margin-x.psw-mobile-p-up-2.psw-tablet-p-up-3.psw-tablet-l-up-6 div")){
-
-                if(row.select(".psw-p-t-2xs.psw-truncate-text-2.psw-body-2").text().equals(""))
+            for(Element row : document.select("div.style-scope.ytd-rich-grid-renderer div")){
+                if(row.select("#meta").text().equals(""))
                 {
                     continue;
-                }
-                else
-                {
-                    final String ticker = row.select(".psw-p-t-2xs.psw-truncate-text-2.psw-body-2").text();
-                    final String price = row.select(".price__container").text();
-                    System.out.println(ticker + "           " + price);
+                } else {
+                    final String ticker = row.select("#meta").text();
 
+                    System.out.println(ticker);
                 }
-
             }
 
         }
-
-        // catch
-        catch(Exception e) {
+        catch(Exception e){
             e.printStackTrace();
         }
 
